7ad97cbb8bbff67636dd89c8eb04896ffeae318b
ysk <103978285+infinitumwrld@users.noreply.github.com>
2023-02-02 16:45:59 -0500

    Update connecting.js

--- connecting.js
+++ connecting.js
@@ -2,3 +2,6 @@ console.log('I know javascript like the palm of my hand') // lol
 
 console.log('I think you lying') // nah
 
+function tellJoke() {
+  return "Why did the tomato turn red? Because it saw the salad dressing!";
+}
